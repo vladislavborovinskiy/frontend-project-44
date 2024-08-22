26dfec2706517775dfa9710e4c98969e3852fddd
Your Name <you@example.com>
2024-08-22 20:07:16 +0300

    fix progression.js

--- src/games/progression.js
+++ src/games/progression.js
@@ -9,7 +9,7 @@ const generateProgression = (increment, start, length) => {
   return progression;
 };
 
-const findMissingNumber = (progression) => {
+const findMissingNumber = (progression, increment) => {
   const replacingElement = '..';
   const randomIndex = getRandomInRange(0, progression.length - 1);
   const newProgression = [...progression];
@@ -19,19 +19,19 @@ const findMissingNumber = (progression) => {
   if (randomIndex === 0) {
     correctAnswer = newProgression[1] - newProgression[0];
   } else {
-    correctAnswer = newProgression[randomIndex - 1] + newProgression[1] - newProgression[0];
+    correctAnswer = newProgression[randomIndex - 1] + increment;
   }
 
-  return [newProgression.join(' '), correctAnswer.toString()];
+  return [newProgression.join(' '), String(correctAnswer)];
 };
 
 const generateRound = () => {
-  const length = getRandomInRange(5, 8);
-  const start = getRandomInRange(1, 10);
+  const length = getRandomInRange(5, 10);
+  const start = getRandomInRange(1, 100);
   const increment = getRandomInRange(2, 10);
 
   const progression = generateProgression(increment, start, length);
-  return findMissingNumber(progression);
+  return findMissingNumber(progression, increment);
 };
 
 export default () => {
